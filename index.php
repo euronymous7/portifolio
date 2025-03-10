c1e477d22def9040c6715aac10e8c8bcaaf32c58
jpedutra1607 <joaopedrodutra160707@gmail.com>
2025-03-10 20:48:13 -0300

    10/03/25

--- index.php
+++ index.php
@@ -1,54 +1,65 @@
 <!DOCTYPE html>
-<html lang="pt-BR">
+<html lang="pt-br">
 <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>portifolio</title>
 
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PORTIFOLIO</title>
-
-    <link rel="stylesheet" href="css/estilos>
-
-
-
+    <link rel="stylesheet" href="tools/fontawesome/css/all.css" />
+    <link rel="stylesheet" href="css/estilos.css">
 </head>
 <body>
-    <div class="interface">
 
-        <header class="cabecalho">
 
-            <nav class="menu">
+<div id="interface">
 
-                <div class="logo">
+    <header class="cabecalho">
 
-                    <img src="imagens/logo.png" alt='LOGO' width='400'>
-                </div>
-                <ul>
-                    <li>Home</li>
-                    <li>Serviços</li>
-                    <li>Produtos</li>
-                    <li>Portfólio</li>
-                    <li>Sobre</li>
-                    <li>Contato</li>
-                </ul>
-            </nav>
+    
+
+        <nav class="menu">
+
+            <div class="logo">
+                <img src="imagens/logo.png" alt="LOGO" width="150" />
+            </div>
+
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Serviços</a></li>
+                <li><a href="#">Produtos</a></li>
+                <li><a href="#">Portifolio</a></li>
+                <li><a href="#">Sobre</a></li>
+                <li><a href="#">Contato</a></li>
+                
+            </ul>
+
+        </nav>
+
+        <div class="titles">
+
+     <div class="container-titles">
+
+            <h1 class="titulo">Entregando Serviços de Qualidade</h1>
+            <h1 class="subtitulo">Soluções em TI</h1>
+
+            <p class="paragrafo">Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam quibusdam ab in! A, nam voluptas!</p>
+
+
         
+         </div>
 
-            <div class="titles">
-                <h1 class="title">Entregando Serviços de Qualidade</h1>
-                <h1 class="subtitle">Soluções em T.I</h1>
-                <p class='paragraph'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur natus obcaecati, illo doloribus iusto sit numquam nam laboriosam?</p>
+             <div class="socialIcons">
+              <a href="#"title="visite nosso twitter"><i class="fab fa-twitter"></i></a>
+              <a href="#"title="visite nosso facebook"><i class="fab fa-facebook"></i></a>
+              <a href="#"title="visite nosso linked"><i class="fab fa-linkedin"></i></a>
+              <a href="#"title="visite nosso instagram"><i class="fab fa-instagram"></i></a>
             </div>
 
-            <div class="socialIcons">
-                <a href="#" target="_blank">Twitter</a>
-                <a href="#">Facebook</a>
-                <a href="#">LinkedIn</a>
-                <a href="#">Instagram</a>
-            </div>
-        </header>
+         </div>
 
-    </div>
+    </header>
 
-   
+</div>
+    
 </body>
 </html>
